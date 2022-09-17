5b37d1f2fd3886566753c9515cf73f4e8390c897
Mat <mateuscolvr@gmail.com>
2022-09-17 12:08:37 -0300

    venusaur

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import { useEffect, useState } from "react"
-import PokeApi from "./api/pokemonList"
-import Pokemon from "./Pokemon"
-import "./index.css"
+import { useEffect, useState } from "react";
+import PokeApi from "./api/pokeApi";
+import Pokemon from "./Pokemon";
+import "./index.css";
 
-export default function() {
-  const [pokemonList, setPokemonList] = useState([])
+export default function () {
+  const [pokemonList, setPokemonList] = useState([]);
   useEffect(() => {
-    PokeApi.all().then(data => setPokemonList(data))
-    }, [])
-  return ( 
-  <div className="flex justify-center items-center min-h-screen gap-2 flex-wrap p-4">
-    {pokemonList.map(data => {
-      const pkm = data.species[0].pokemons[0];
-      const sprite = JSON.parse(pkm.spriteList[0].sprites).other["official-artwork"].front_default;
-      const types = pkm.types.map(valor => {
-          return valor.type.name
-        })
-       return <Pokemon key={pkm.id} name={pkm.name} cover={sprite} height={pkm.height} pokeNumber={pkm.id} pokeTypes={types} />
+    PokeApi.all().then((data) => setPokemonList(data));
+  }, []);
+  return (
+    <div className="flex justify-center items-center min-h-screen gap-2 flex-wrap p-4">
+      {pokemonList.map((data) => {
+        const pkm = data.species[0].pokemons[0];
+        const sprite = JSON.parse(pkm.spriteList[0].sprites).other[
+          "official-artwork"
+        ].front_default;
+        const types = pkm.types.map((value) => {
+          return value.type.name;
+        });
+        return (
+          <Pokemon
+            key={pkm.id}
+            name={pkm.name}
+            cover={sprite}
+            height={pkm.height}
+            pokeNumber={pkm.id}
+            pokeTypes={types}
+          />
+        );
       })}
-  </div>
-  )
+    </div>
+  );
 }
-
