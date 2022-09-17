6e227e76bca60d243fb5b1f66edfbd41ee99ba4d
Mat <mateuscolvr@gmail.com>
2022-09-17 10:45:21 -0300

    ivysaur

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { useEffect, useState } from "react"
-import client from "./api/client"
+import PokeApi from "./api/pokemonList"
 import Pokemon from "./Pokemon"
 import "./index.css"
 
 export default function() {
   const [pokemonList, setPokemonList] = useState([])
   useEffect(() => {
-    client().then(data => setPokemonList(data))
+    PokeApi.all().then(data => setPokemonList(data))
     }, [])
   return ( 
   <div className="flex justify-center items-center min-h-screen gap-2 flex-wrap p-4">
     {pokemonList.map(data => {
-      const pkm = data.pokemon_v2_pokemonspecies[0].pokemon_v2_pokemons[0];
-      const sprite = JSON.parse(pkm.pokemon_v2_pokemonsprites[0].sprites).other["official-artwork"].front_default;
-      const types = pkm.pokemon_v2_pokemontypes.map(valor => {
-          return valor.pokemon_v2_type.name
+      const pkm = data.species[0].pokemons[0];
+      const sprite = JSON.parse(pkm.spriteList[0].sprites).other["official-artwork"].front_default;
+      const types = pkm.types.map(valor => {
+          return valor.type.name
         })
        return <Pokemon key={pkm.id} name={pkm.name} cover={sprite} height={pkm.height} pokeNumber={pkm.id} pokeTypes={types} />
       })}
--- src/api/client.ts
+++ src/api/client.ts
@@ -1,32 +1,9 @@
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client"
+import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 const client = new ApolloClient({
   uri: 'https://beta.pokeapi.co/graphql/v1beta',
   cache: new InMemoryCache(),
 });
 
-const GetPokemons = gql`
-query GetPokemons {
-  pokemon_v2_evolutionchain {
-    pokemon_v2_pokemonspecies(limit: 1) {
-      pokemon_v2_pokemons {
-        id
-        name
-        height
-        pokemon_v2_pokemonsprites {
-          sprites
-        }
-        pokemon_v2_pokemontypes {
-          pokemon_v2_type {
-            name
-          }
-        }
-      }
-    }
-  }
-}
-`
 
-export default async function() {
-  return (await client.query({query: GetPokemons})).data.pokemon_v2_evolutionchain
-}
+export default client
